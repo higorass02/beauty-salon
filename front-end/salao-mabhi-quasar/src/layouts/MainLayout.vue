38ffb08f821141ae5a2ba3ab6273896b23312898
higor Santos <higoraugusto120@gmail.com>
2023-09-20 23:06:10 -0300

    quasar run and get API mock

--- front-end/salao-mabhi-quasar/src/layouts/MainLayout.vue
+++ front-end/salao-mabhi-quasar/src/layouts/MainLayout.vue
@@ -14,8 +14,6 @@
         <q-toolbar-title>
           Quasar App
         </q-toolbar-title>
-
-        <div>Quasar v{{ $q.version }}</div>
       </q-toolbar>
     </q-header>
 
@@ -28,7 +26,7 @@
         <q-item-label
           header
         >
-          Essential Links
+          Menus
         </q-item-label>
 
         <EssentialLink
@@ -51,46 +49,10 @@ import EssentialLink from 'components/EssentialLink.vue'
 
 const linksList = [
   {
-    title: 'Docs',
-    caption: 'quasar.dev',
-    icon: 'school',
-    link: 'https://quasar.dev'
-  },
-  {
-    title: 'Github',
-    caption: 'github.com/quasarframework',
-    icon: 'code',
-    link: 'https://github.com/quasarframework'
-  },
-  {
-    title: 'Discord Chat Channel',
-    caption: 'chat.quasar.dev',
-    icon: 'chat',
-    link: 'https://chat.quasar.dev'
-  },
-  {
-    title: 'Forum',
-    caption: 'forum.quasar.dev',
-    icon: 'record_voice_over',
-    link: 'https://forum.quasar.dev'
-  },
-  {
-    title: 'Twitter',
-    caption: '@quasarframework',
-    icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev'
-  },
-  {
-    title: 'Facebook',
-    caption: '@QuasarFramework',
-    icon: 'public',
-    link: 'https://facebook.quasar.dev'
-  },
-  {
-    title: 'Quasar Awesome',
-    caption: 'Community Quasar projects',
-    icon: 'favorite',
-    link: 'https://awesome.quasar.dev'
+    title: 'Teste link',
+    caption: '',
+    icon: 'home',
+    route: { name: 'home' }
   }
 ]
 
--- front-end/salao-mabhi-quasar/src/pages/IndexPage.vue
+++ front-end/salao-mabhi-quasar/src/pages/IndexPage.vue
@@ -1,17 +1,45 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
+  <q-page padding>
+    <q-table
+      title="Treats"
+      :rows="posts"
+      :columns="columns"
+      row-key="name"
+    />
   </q-page>
 </template>
 
 <script>
-import { defineComponent } from 'vue'
+import { defineComponent, onMounted, ref } from 'vue'
+import { api } from 'boot/axios'
 
 export default defineComponent({
-  name: 'IndexPage'
+  name: 'IndexPage',
+  setup () {
+    const posts = ref([])
+    const columns = [
+      { name: 'id', field: 'id', label: 'Id', sortable: true, align: 'left' },
+      { name: 'title', field: 'title', label: 'Titulo', sortable: true, align: 'left' },
+      { name: 'author', field: 'author', label: 'Autor', sortable: true, align: 'left' }
+    ]
+
+    onMounted(() => {
+      getPosts()
+    })
+
+    const getPosts = async () => {
+      try {
+        const response = await api.get('posts')
+        posts.value = response.data
+      } catch (error) {
+        console.log(error)
+      }
+    }
+
+    return {
+      posts,
+      columns
+    }
+  }
 })
 </script>
